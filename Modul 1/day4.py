d2136f48e32f38d133c843cefd8f5f3ac2c56895
Abdillah Zaraaifa A <abdillah.zaraa@gmail.com>
2025-02-15 23:02:02 +0700

    Delete Modul 1 directory

--- Modul 1/day4.py
+++
@@ -1,6 +0,0 @@
-for angka in range(10):
-    print(angka)
-    if angka % 5 == 0:
-        print(f'Angka {angka} merupakan kelipatan 5')
-    else:
-        print(f'Angka {angka} tidak merupakan kelipatan 5')
