9fdcaf00373eac5549340b65a3acc9d5ce7a74d1
Abdillah Zaraaifa A <abdillah.zaraa@gmail.com>
2025-02-15 23:01:50 +0700

    Delete JCDS_0610 directory

--- JCDS_0610/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-from .fungsi_add import tambah_data
-from .fungsi_update import update_data
-from .fungsi_delete import hapus_data
-from .fungsi_show import tampilkan_data
-from .fungsi_restore import restore_data
-from .fungsi_cek import cek_status
